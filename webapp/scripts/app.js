7694b3e8179dfab576b2ab55fcf007515a482ae6
RekenaarGenie <150270674+RekenaarGenie@users.noreply.github.com>
2024-08-26 00:58:27 +0300

    Update app.js

--- webapp/scripts/app.js
+++ webapp/scripts/app.js
@@ -1,1 +1,17 @@
+// Получаем все элементы внутри .footer_navigation
+const navItems = document.querySelectorAll('.footer_navigation > div');
 
+// Функция для удаления класса active_screen у всех элементов
+function removeActiveClass() {
+    navItems.forEach(item => {
+        item.classList.remove('active_screen');
+    });
+}
+
+// Добавляем обработчик события клика для каждого элемента
+navItems.forEach(item => {
+    item.addEventListener('click', function() {
+        removeActiveClass(); // Удаляем класс active_screen у всех элементов
+        this.classList.add('active_screen'); // Добавляем класс текущему элементу
+    });
+});
